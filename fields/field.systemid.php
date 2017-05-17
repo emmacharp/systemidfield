87becf5452f849ed19e5ad52b10bf54e8d143103
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-17 13:51:35 -0400

    Fix compatibility warnings for PHP7

--- fields/field.systemid.php
+++ fields/field.systemid.php
@@ -62,8 +62,7 @@
 		Settings:
 	-------------------------------------------------------------------------*/
 
-		public function displaySettingsPanel(XMLElement &$wrapper, $errors = null)
-		{
+		public function displaySettingsPanel(XMLElement &$wrapper, $errors = null) {
 			parent::displaySettingsPanel($wrapper, $errors);
 			$this->appendShowColumnCheckbox($wrapper);
 		}
@@ -72,8 +71,7 @@
 		Publish:
 	-------------------------------------------------------------------------*/
 
-		public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null)
-		{
+		public function displayPublishPanel(XMLElement &$wrapper, $data = null, $flagWithError = null, $fieldnamePrefix = null, $fieldnamePostfix = null, $entry_id = null) {
 			$label = Widget::Label($this->get('label'));
 			$label->appendChild(
 				Widget::Input(
@@ -91,15 +89,13 @@
 		Input:
 	-------------------------------------------------------------------------*/
 
-		public function checkPostFieldData($data, &$message, $entry_id = null)
-		{
+		public function checkPostFieldData($data, &$message, $entry_id = null) {
 			$message = null;
 
 			return self::__OK__;
 		}
 
-		public function processRawFieldData($data, &$status, &$message = null, $simulate = false, $entry_id = null)
-		{
+		public function processRawFieldData($data, &$status, &$message = null, $simulate = false, $entry_id = null) {
 			$status = self::__OK__;
 
 			return $data;
@@ -109,8 +105,7 @@
 		Output:
 	-------------------------------------------------------------------------*/
 
-		public function appendFormattedElement(XMLElement &$wrapper, $data, $encode = false, $mode = null, $entry_id = null)
-		{
+		public function appendFormattedElement(XMLElement &$wrapper, $data, $encode = false, $mode = null, $entry_id = null) {
 			$element = new XMLElement($this->get('element_name'));
 			$element->setAttribute('hash', @dechex($data['value']));
 			$element->setValue(@$data['value'] ? $data['value'] : '0');
@@ -126,8 +121,7 @@
 		Filtering:
 	-------------------------------------------------------------------------*/
 
-		public function displayDatasourceFilterPanel(XMLElement &$wrapper, $data = null, $errors = null, $prefix = null, $postfix = null)
-		{
+		public function displayDatasourceFilterPanel(XMLElement &$wrapper, $data = null, $errors = null, $prefix = null, $postfix = null) {
 			$wrapper = new XMLElement('p');
 			$wrapper->setAttribute('style', 'display: none;');
 		}
